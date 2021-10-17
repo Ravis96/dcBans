dcb3a87f31a0fd55987c7d7fde5da09066df24c0
Ravis96 <ravis96@o2.pl>
2021-10-17 12:47:05 +0200

    Wprowadzenie event'ow dla dev. ✅

---
+++ src/main/java/pl/dreamcode/dcbans/events/PlayerMuteEvent.java
@@ -0,0 +1,50 @@
+package pl.dreamcode.dcbans.events;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import pl.dreamcode.dcbans.user.User;
+import pl.dreamcode.dcbans.user.ban.Ban;
+
+@Getter
+@RequiredArgsConstructor
+public class PlayerMuteEvent extends Event implements Cancellable {
+    private final HandlerList handlerList = new HandlerList();
+    private final User user;
+    private final Ban ban;
+    private String reason = defaultReason();
+    private boolean cancelled;
+
+    public User getUser() {
+        return this.user;
+    }
+
+    public Ban getBan() {
+        return this.ban;
+    }
+
+    public void setCancelledReason(String text) {
+        this.reason = text;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return handlerList;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancelled;
+    }
+
+    @Override
+    public void setCancelled(boolean cancelled) {
+        this.cancelled = cancelled;
+    }
+
+    private String defaultReason() {
+        return "&cEvent zostal anulowany!";
+    }
+}
